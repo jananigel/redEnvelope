a812d90018843a25a84c9468452882618dbe87b1
Kyle <jananigel@hotmail.com>
2017-01-25 15:30:15 +0800

    add the link of facebook and website

--- js/main.js
+++ js/main.js
@@ -14,13 +14,19 @@ $(function(){
 	});*/
 	_imgName = "";
 	_footerImgW = 0;
-	
 
-	_footerImgW = (($('.footer').width() / 2) - 10) * -1;
+	var invocation = new XMLHttpRequest();
+	var url = "http://60.250.132.191/NHIS2017/Api/Json/AreaJson"
+	$.get('http://60.250.132.191/NHIS2017/Api/Json/AreaJson',
+	 function(data){
+		console.log(data.query);
+	});
+
+	_footerImgW = (($('.footer').width() / 2)-50) * -1;
 	$('.footer').css({
 		marginLeft: _footerImgW
 	});
-	
+
 	$('a.redEnv').click(function(){
 		$('.modal-body img').attr('src', 'img/src/' + $(this).children('img').attr('src').substr(4));
 		_imgName = 'img/src/' + $(this).children('img').attr('src').substr(4);
@@ -33,20 +39,41 @@ $(function(){
 	    a[0].click();
 	    a.remove();
 	});
+
 	$('.main-pic').fadeIn('500');
 	$('.pull-left').delay('500').fadeIn('500');
 	$('.pull-right').delay('1000').fadeIn('500');
 	$('.content').delay('1500').fadeIn('500');
+	$('.bottom-left,.bottom-right').delay('2000').fadeIn('500');
 	$('.bottom').delay('2000').animate({
 		opacity: 1
 	}, 500 , function(){
 
 	});
-})
+	/*$('.bottom').delay('2000').fadeIn('500' , function(){
+		_footerImgW = (($('.footer').width() / 2)-50) * -1;
+		$('.footer').css({
+			marginLeft: _footerImgW
+		});
+	});*/
+	$('.closeBtn').click(function(e){
+		console.log('close');
+		$('.border').css({
+			width: '0%'
+		});
+	})
+	$('.openBtn').click(function(e){
+		console.log('open');
+		$('.border').css({
+			width: '240px'
+		});
+	})
+	
+});
 
 $(window).resize(function(){
-	_footerImgW = (($('.footer').width() / 2) - 10 ) * -1;
+	_footerImgW = (($('.footer').width() / 2)-50 ) * -1;
 	$('.footer').css({
 		marginLeft: _footerImgW
 	});
-})
+});
